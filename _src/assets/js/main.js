824d6712753d262d38bcb23e3c2eb595e1e934e0
“Sara <saracrojas@gmail.com>
2019-04-08 09:07:01 +0200

    Add js

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,3 +1,30 @@
 'use strict';
 
+
+const counter = document.querySelector('.page__main-counter');
+const insertNumber = document.querySelector('.page__main-insert');
+const btn = document.querySelector('.btn');
+const clue = document.querySelector('.page__main-clue');
+
 console.log('>> Ready :)');
+
+// Función número aleatorio
+
+function getRandomNumber(max) {
+    return Math.ceil(Math.random() * 100);
+  }
+  
+  // Anda, chacho, píntame en la consola un número random hasta 100;
+  console.log('> ' + getRandomNumber(100));
+
+
+
+// Crear función del listener
+
+const writeNumber = () => {
+    counter.innerHTML = insertNumber.value;
+}
+
+//Listener al botón
+
+btn.addEventListener('click', writeNumber);
